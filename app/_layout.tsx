1f6933d5c9e37daf88c158a764e8ebc5348ff859
Z.Ngomane <zngomane023a@student.wethinkcode.co.za>
2024-10-03 03:57:59 +0200

    updating screens - auth and folders arrangement

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,21 @@ import '../global.css';
 
 import { Stack } from 'expo-router';
 
+import AuthContextProvider from '~/contexts/AuthContext';
+
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
 export default function RootLayout() {
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <AuthContextProvider>
+      <Stack>
+        <Stack.Screen name="(app)/(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(app)/modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </AuthContextProvider>
   );
 }
